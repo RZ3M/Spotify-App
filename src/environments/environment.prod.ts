b772a16be39f923c95d28ce4304e5e75437fcf73
Jack <omnomnomny@gmail.com>
2024-11-22 23:50:04 -0500

    prod api updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   clientID: '27fe4e9fd83e47e88d75858ed80afaae',
   clientSecret: '8ff6441f63b842f19c53ce050cf40cba',
-  userAPIBase: 'https://jm-spotify-user-api.herokuapp.com/api/user',
+  userAPIBase: 'https://spoti-api.onrender.com/api/user',
 };
